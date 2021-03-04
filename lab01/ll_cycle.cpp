8aaa9ee5cda68def6ed490e11e118b0e399478d9
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-03-04 15:32:41 +0300

    Completed the 5th task

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -2,5 +2,18 @@
 
 bool ll_has_cycle(node *head) {
     /* Ваш код должен быть написан только внутри этой функции */
-    return true;
+    	node *rabbit;
+	if (head->next != 0 && head->next->next != 0) 
+	{
+		rabbit = head->next->next;
+	}
+    	else return false;
+	node *turtle = head->next;
+	while(rabbit->next->next != 0 && turtle->next != 0)
+    	{
+		turtle = turtle->next;
+	    	rabbit = rabbit->next->next;
+	    	if (rabbit == turtle) return true;
+    	}
+	return false;
 }
