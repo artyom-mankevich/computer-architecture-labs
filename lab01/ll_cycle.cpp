09fd6b2e0e45c815aa9f2cbbaa9cc3f2387ad727
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-03-11 13:16:36 +0300

    Fixed task 5 in lab 1

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -3,17 +3,20 @@
 bool ll_has_cycle(node *head) {
     /* Ваш код должен быть написан только внутри этой функции */
     	node *rabbit;
-	if (head->next != 0 && head->next->next != 0) 
+	if (head != 0 && head->next != 0 && head->next->next != 0) 
 	{
 		rabbit = head->next->next;
 	}
     	else return false;
 	node *turtle = head->next;
-	while(rabbit->next->next != 0 && turtle->next != 0)
+	while(rabbit != 0 && rabbit->next != 0 && rabbit->next->next != 0 && turtle != 0 && turtle->next != 0)
     	{
 		turtle = turtle->next;
 	    	rabbit = rabbit->next->next;
-	    	if (rabbit == turtle) return true;
+	    	if (rabbit == turtle)
+		{
+		       	return true;
+		}
     	}
 	return false;
 }
