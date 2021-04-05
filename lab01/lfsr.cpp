48fedd9145541425020a2b5f5d578d5e98540c38
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-04-05 18:09:52 +0300

    Minor fixes 1st lab

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    *reg = ((((*reg >> 5) ^ (*reg >> 3) ^ (*reg >> 2) ^ *reg) & 0x00000001) << 15) | (*reg >> 1);
+    *reg = ((((*reg >> 5) ^ (*reg >> 3) ^ (*reg >> 2) ^ *reg) & 1) << 15) | (*reg >> 1);
 }
 
